b83f5da69496deb25d7d94fe896ddaf60be81811
Nawal Nadeem <nawal.nadeem@gmail.com>
2024-06-26 13:22:48 +0500

    Create rampcalculator.py

---
+++ rampcalculator.py
@@ -0,0 +1,5 @@
+height= float(input("Enter the height of the ramp "))
+ratio= float(input("Enter the ratio of the ramp 1: "))
+unit=input("Enter your measurement unit. 'ft' for feet or 'm' for meters ")
+ramp_length= height*ratio
+print("The ramp lenght should be", ramp_length, unit)
